4fde7bf6415f4696aa1e5a0cc9d2848b7f7cee65
Adrian <dielorg@gmail.com>
2019-05-10 14:55:04 +0200

    file reader

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    new Main().run();
+    }
+
+    public static void run(){
+        try {
+            Scanner scanner = new Scanner(new File("src/input.txt"));
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
